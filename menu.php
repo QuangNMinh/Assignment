356a05858c00517b454533dcdfd3ca9220ba0385
QuangNMinh <57930519+QuangNMinh@users.noreply.github.com>
2019-12-28 14:29:43 +0700

    Update menu.php

--- menu.php
+++ menu.php
@@ -2,7 +2,7 @@
     <ul>
         <li>
                 <li><a href="insertProduct.php">Add a product</a></li>
-                <li><a href="View.php">Manage products</a></li>
+                <li><a href="view.php">Manage products</a></li>
         </li>
     </ul>
 </nav>
