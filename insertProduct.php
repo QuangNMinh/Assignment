da36ddbcc7c81e92a3445a13bdad39c862e555fc
QuangNMinh <57930519+QuangNMinh@users.noreply.github.com>
2019-12-28 14:28:07 +0700

    Add files via upload

--- insertProduct.php
+++ insertProduct.php
@@ -1,5 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Create Product</title>
+</head>
 <?php
-require_once 'menu.php';
+
     $db = parse_url(getenv("DATABASE_URL"));
     $pdo = new PDO("pgsql:" . sprintf(
         "host=%s;port=%s;user=%s;password=%s;dbname=%s",
@@ -21,3 +26,15 @@ require_once 'menu.php';
     echo("insert ok!");
  
 ?>
+
+<body>
+<h1>Create Product </h1>
+    <form action="insertProduct.php" method="post">
+        Name <input type="text" name="name" >
+        <br>
+        Price <input type="text" name="price">
+        <br>
+        <input type="submit" value="Insert">
+    </form>
+</body>
+</html>
