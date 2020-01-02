e0c2b72c4ceb510e65f2b712ce19dd6b40b1c29e
QuangNMinh <57930519+QuangNMinh@users.noreply.github.com>
2020-01-02 11:59:40 +0700

    Add files via upload

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
-include("menu.php"); 
+
  include("db.php");
 session_start();
 
--- menu.php
+++ menu.php
@@ -1,6 +1,7 @@
 <div>
 <nav>
     <ul>
+    			<li><a href="index.php">Homepage</a></li>
                 <li><a href="insertProduct.php">Add a product</a></li>
                 <li><a href="view.php">Manage products</a></li>
                 <li><a href="login.php">Log in</a></li>
