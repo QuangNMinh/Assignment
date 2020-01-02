fb497aa2faf1813f9e2cbd99dc5665cf667d878c
QuangNMinh <57930519+QuangNMinh@users.noreply.github.com>
2020-01-02 10:19:59 +0700

    Update index.php

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ if(isset($_SESSION['sess_user_id']) && $_SESSION['sess_user_id'] != "") {
   echo '<h4><a href="logout.php">Logout</a></h4>';
 } else { 
   header('location:index.php');
+}
  ?>
 </body>
 </html>
