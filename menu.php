1e0545acbd805d6362a4aa8a2b21712eb713264a
QuangNMinh <57930519+QuangNMinh@users.noreply.github.com>
2020-01-02 11:55:49 +0700

    Add files via upload

--- menu.php
+++ menu.php
@@ -3,6 +3,7 @@
     <ul>
                 <li><a href="insertProduct.php">Add a product</a></li>
                 <li><a href="view.php">Manage products</a></li>
+                <li><a href="login.php">Log in</a></li>
     </ul>
 </nav>
 </div>
