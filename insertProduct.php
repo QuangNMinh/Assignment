dc5b9ad7d0be92a4f416bb94b701cd414d92ca90
QuangNMinh <57930519+QuangNMinh@users.noreply.github.com>
2020-01-03 11:18:42 +0700

    Update insertProduct.php

--- insertProduct.php
+++ insertProduct.php
@@ -18,8 +18,6 @@ include("index.php");
 
 <h1>Create Product </h1>
     <form action="insertProduct.php" method="post">
-        <div class="error"><?php echo $error; ?></div>
-        <div class="msg"><?php echo $msg; ?></div>
         Product Name: <input type="text" name="name" >
         <br>
         Price: <input type="text" name="price">
@@ -29,9 +27,9 @@ include("index.php");
         $pdo->prepare("INSERT INTO products(name, price) VALUES (:name,:price)");   
     $stmt->execute($data);
     if (!$stmt) {
-        $error = $error . "<br>Can't add product, please try again";
+        echo "<br>Can't add product, please try again";
     } else {
-        $msg = "Added successfully!";
+        echo "<br>Added successfully!;
     } ?>
     
     </form>
