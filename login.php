13cbad00f2948e50988686a237584da7a3703cde
QuangNMinh <57930519+QuangNMinh@users.noreply.github.com>
2020-01-03 11:14:00 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,29 +1,30 @@
 <html>
 <body>
+<?php 
+ include("db.php");
+$error = $user = $pass = "";
+    $user = $_POST["user"];
+    $pass = $_POST["pass"];
+    if ($user == "" || $pass == "") {
+        $error = "Not all fields was entered";
+    } else {
+    	$data = [
+        'user' => $user,
+        'pass' => $pass
+    ];
+	}
+?>
+
 <br>
 <form method="post" action="login.php">
     <fieldset class="fitContent">
-        <legend>Please login</legend>
-        <span class="error"><?php echo $error ?></span><br>
+        <legend>Please log in</legend>
         Username: <br>
         <input type="text" name="user" value=""/><br>
         Password : <br>
         <input type="password" name="pass" value=""/><br>
         <input type="submit" value="Login"/>
         <?php
-        include("db.php");
-		$error = $user = $pass = "";
-			$user = $_POST["user"];
-    		$pass = $_POST["pass"];
-    		if ($user == "" || $pass == "") {
-        		$error = "Not all fields was entered";
-        		} 
-        		else 
-        		{
-    			$data = [
-        			'user' => $user,
-        			'pass' => $pass];
-				};
         $stmt = $pdo->prepare("select * from users where username=:user and password=:pass");   
     	$stmt->execute($data);
     	$count = $stmt->rowCount();
@@ -37,6 +38,7 @@
             die("You already log in. Please <a href='index.php'>click here</> to continue.");
         }
         ?>
+        <span class="error"><?php echo $error ?></span><br>
     </fieldset>
 </form>
 </body>
