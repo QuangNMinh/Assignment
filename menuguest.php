3c37c6170d9a38339651b62a827292864b617cb4
QuangNMinh <57930519+QuangNMinh@users.noreply.github.com>
2020-01-04 10:01:52 +0700

    Update menuguest.php

--- menuguest.php
+++ menuguest.php
@@ -1,11 +1,11 @@
-<link rel="stylesheet" type="text/css" href="menu_sytle.css">
+<link rel="stylesheet" type="text/css" href="menu_style.css">
 <nav> 
     <ul>
         <li>
             <a href="index.php">Home</a>
         </li>
         <li>
-            <a href="login.php">Login></a>
+            <a href="login.php">Login</a>
         </li>
     </ul> 
 </nav>
