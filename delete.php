64c2eb1af33839da72943bf76db72736c550af0c
QuangNMinh <57930519+QuangNMinh@users.noreply.github.com>
2020-01-04 12:28:19 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -9,5 +9,5 @@
      $stmt =  
          $pdo->prepare("delete from products where id = :id");	
      $stmt->execute($data);
-     echo("delete ok!");
+     echo("delete ok!Please <a href='index.php'>click here</> to continue");
 ?>
